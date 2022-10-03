0227cff8b6d06b2cf7a0b5cd3a1a1ca20edbd367
Praveen Rao V P <praveenraovp01@gmail.com>
2022-10-03 11:34:38 +0530

    added styled as default styles manager

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,26 @@ import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import { Actors, MovieInformation, Movies, NavBar, Profile } from './';
-
+import { MainComponent, Toolbar, Content }  from './styles';
 
 const App = () => {
+
+
   return (
-    <div>
+    <MainComponent>
         <CssBaseline />
         <NavBar />
-        <main>
-          <Routes>
-            <Route exact path="/" element={<Movies />} />
-            <Route exact path="/actors/:id" element={<Actors />} />
-            <Route exact path="/movie/:id" element={<MovieInformation />} />
-            <Route exact path="/profile/:id" element={<Profile />} />
-          </Routes>
-        </main>
-    </div>
+        <Content>
+          <Toolbar>
+            <Routes>
+              <Route exact path="/" element={<Movies />} />
+              <Route exact path="/actors/:id" element={<Actors />} />
+              <Route exact path="/movie/:id" element={<MovieInformation />} />
+              <Route exact path="/profile/:id" element={<Profile />} />
+            </Routes>
+          </Toolbar>
+        </Content>
+    </MainComponent>
   )
 }
 
---
+++ src/components/styles.js
@@ -0,0 +1,17 @@
+import { styled } from "@mui/system";
+
+export const MainComponent = styled('div')({
+    display: 'flex',
+    height: '100%',
+});
+
+export const Toolbar = styled('div')({
+    height: '70px',
+});
+
+export const Content = styled('div')({
+    flexGrow: '1',
+    padding: '2em',
+});
+
+//export default MainComponent;
