2f7a1b209da56c1c3b8a3c51d1b6cfce9618bca9
Praveen Rao V P <praveenraovp01@gmail.com>
2022-11-10 09:59:17 +0530

    env removed

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -12,6 +12,7 @@ const Movie = ({ movie, idx}) => {
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
     
     <Grow in key={i} timeout={2500*itr++}>
+    {/* Display thumbnail and title with ratings  */}
       <Typography className={classes.title} variant="h5">{movie.title}</Typography>
     </Grow>
     </Grid>
