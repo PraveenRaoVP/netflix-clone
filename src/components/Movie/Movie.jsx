78291beff25b6520789b7a7c8dd247486f62fa95
Praveen Rao V P <praveenraovp01@gmail.com>
2023-04-19 19:32:54 +0530

    new commit

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -3,18 +3,17 @@ import { Typography, Grid, Grow, Tooltip, Rating } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useStyles } from './styles';
 
-const Movie = ({ movie, idx}) => {
+const Movie = ({ movie, idx }) => {
     const i = idx;
     var itr = 1
     const { classes } = useStyles();
     // console.log(movie,idx)
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
-    
-    <Grow in key={i} timeout={2500*itr++}>
-    {/* Display thumbnail and title with ratings  */}
-      <Typography className={classes.title} variant="h5">{movie.title}</Typography>
-    </Grow>
+      <Grow in key={i} timeout={2500*itr++}>
+      {/* Display thumbnail and title with ratings  */}
+        <Typography className={classes.title} variant="h5">{movie.title}</Typography>
+      </Grow>
     </Grid>
   )
 }
